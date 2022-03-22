e0f7e60485b86b9fc1e2ee6652372e7da2e792a6
lesantxs <l.sancarmo@gmail.com>
2022-03-22 00:48:45 -0300

    emulando banco de dados

--- src/controllers/funcionarios-controller.js
+++ src/controllers/funcionarios-controller.js
@@ -1,14 +1,47 @@
-const funcionarioController = (app) => {
+import Funcionarios from "../models/Funcionarios.js";
+
+const funcionarioController = (app, bd) => {
   app.get("/funcionarios", (req, res) => {
+    const todosFuncionarios = bd.funcionarios
+
     res.json({
-      "funcionarios": [],
-    });
-  });
+      "funcionarios": todosFuncionarios,
+      "erro": false
+    })
+  })
+
+  app.get("/funcionarios/email/:email"), (req, res)=>{
+    const email = req.params.email
+
+    const funcionarioEncontrado = bd.funcionarios.filter(funcionario=>(funcionario.email == email))
+
+    res.json({
+      "funcionario": funcionarioEncontrado,
+      "erro": false
+    })
+  }
 
   app.post("/funcionarios", (req, res) => {
-    res.json({
-      "msg": "Funcionário cadastrado com sucesso",
-    });
+    const body = req.body
+
+    try{
+      const novoFuncionario = new Funcionarios(body.id, body.nome, body.funcao, body.email, body.senha, body.status, body.demissao)
+
+      bd.funcionarios.push(novoFuncionario)
+
+      res.json({
+        "msg": `Funcionário ${novoFuncionario.nome} admitido`,
+        "funcionario": novoFuncionario,
+        "erro": false
+      })
+    }
+    catch(error){
+      res.json({
+        "msg": error.message,
+        "erro": true
+      })
+    }
+
   });
 };
 
