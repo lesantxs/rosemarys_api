ef71a88033a3e5eeea029f0e61e106bd6465b9ab
lesantxs <l.sancarmo@gmail.com>
2022-03-22 00:49:47 -0300

    adicionando imports e chamadas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import express from "express";
+
+//controller
 import funcionarioController from "./controllers/funcionarios-controller.js";
 
+//middleware
+import generalMiddleware from "./middleware/general-middleware.js";
 const app = express();
 const port = 3000;
 
-funcionarioController(app);
+//bd
+import bd from "./database/bd.js"
+
+app.use(express.json())
+
+//middlware
+generalMiddleware(app)
+
+//controller
+funcionarioController(app, bd);
 
 app.listen(port, () => {
   console.log(`Servidor aberto na http://localhost:${port}/`);
