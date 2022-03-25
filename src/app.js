a126adc1032b47cf0245f1af995e48f140494b68
lesantxs <l.sancarmo@gmail.com>
2022-03-25 16:16:45 -0300

    importando cors

--- src/app.js
+++ src/app.js
@@ -9,9 +9,14 @@ import generalMiddleware from "./middleware/general-middleware.js";
 //bd
 import database from "./database/sqlite-db.js";
 
+//cors
+import cors from 'cors'
+
 const app = express();
 
 app.use(express.json())
+
+app.use(cors())
 
 //middlware
 generalMiddleware(app)
