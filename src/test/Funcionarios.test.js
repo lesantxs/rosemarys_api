1876c487ef98d961b26ab206935169eb7ab90dd9
lesantxs <l.sancarmo@gmail.com>
2022-04-23 20:35:49 -0300

    teste post

--- src/test/Funcionarios.test.js
+++ src/test/Funcionarios.test.js
@@ -9,15 +9,28 @@ describe("GET /funcionarios", () => {
 });
 
 describe("GET /funcionario/id/:id", () => {
-    it("Deve retornar todos o funcionário", async () => {
+    it("Deve retornar um funcionário pelo id", async () => {
       const res = await request(app).get("/funcionario/id/3");
       expect(res.statusCode).toEqual(200);
     });
   });
 
   describe("GET /funcionario/email/:email", () => {
-    it("Deve retornar todos o funcionário", async () => {
+    it("Deve retornar um funcionário pelo email", async () => {
       const res = await request(app).get("/funcionario/email/maria.andrade@rosemaryss.com.br");
-      expect(res.statusCode).toEqual(200);
+      expect(res.statusCode).toEqual(201);
+    });
+  });
+
+  describe("POST /funcionario", () => {
+    it("Deve inserir um novo funcionário", async () => {
+      const res = await request(app).post("/funcionario").send({
+        nome: "Eli Gabriel",
+	    cargo: "Adminstrador",
+	    email: "eli.gabriel@rosemaryss.com.br",
+	    senha: "dftgvhjbk656#",
+	    status: "ativo"
+      });
+      expect(res.statusCode).toEqual(201);
     });
   });
